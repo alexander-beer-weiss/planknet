e8dd1a30c845efa0bba717d27be3041c5d55c3f9
mcminis1 <jmcminis@gmail.com>
2015-02-11 09:05:16 -0800

    cleaning up transfer layer code

--- src/convnet.lua
+++ src/convnet.lua
@@ -17,6 +17,13 @@ function convNet.new(opt)
   return self
 end
 
+
+-- in Lua functions are first class citizens
+local trans_layer ={}
+trans_layer['Tanh']=nn.Tanh
+trans_layer['ReLU']=nn.ReLU
+trans_layer['Sigmoid']=nn.Sigmoid
+
 -- Example build
 -- parameters,gradParameters = myNet:build({1,64,64,128,#species}, 2, 5)
 function convNet:build(dimensions, n_pools, n_filter)
@@ -26,20 +33,16 @@ function convNet:build(dimensions, n_pools, n_filter)
   for i=1,n_dimensions-3 do
     if self.dropout[n_dimensions - i] and self.dropout[n_dimensions - i] ~= 0 then self.net:add(nn.Dropout(self.dropout[n_dimensions - i])) end
     self.net:add(nn.SpatialConvolutionMM(dimensions[i], dimensions[i+1], n_filter, n_filter))
-    if self.transfer == 'Tanh' then self.net:add(nn.Tanh())
-	elseif self.transfer == 'ReLU' then self.net:add(nn.ReLU())
-	elseif self.transfer == 'Sigmoid' then self.net:add(nn.Sigmoid()) end
+    self.net:add(trans_layer[self.transfer]())
     self.net:add(nn.SpatialLPPooling(dimensions[i+1],2,n_pools,n_pools,n_pools,n_pools))
     self.net:add(nn.SpatialSubtractiveNormalization(dimensions[i+1], normkernel))
   end
   local linearFeatDim = dimensions[n_dimensions-2] * (((self.height-n_filter+1)/n_pools - n_filter + 1 )/n_pools)  * (((width-n_filter+1)/n_pools - n_filter + 1 )/n_pools)
-  print('feature dim: ' .. linearFeatDim )
+--   print('feature dim: ' .. linearFeatDim )
   self.net:add(nn.Reshape(linearFeatDim))
   if self.dropout[2] and self.dropout[2] ~= 0 then self.net:add(nn.Dropout(self.dropout[1])) end
   self.net:add(nn.Linear(linearFeatDim, dimensions[n_dimensions-1]))
-  if self.transfer == 'Tanh' then self.net:add(nn.Tanh())
-  elseif self.transfer == 'ReLU' then self.net:add(nn.ReLU())
-  elseif self.transfer == 'Sigmoid' then self.net:add(nn.Sigmoid()) end
+  self.net:add(trans_layer[self.transfer]())
   if self.dropout[1] and self.droput[1] ~= 0 then self.net:add(nn.Dropout(self.dropout[2])) end
   self.net:add(nn.Linear(dimensions[n_dimensions-1], dimensions[n_dimensions]))
   self.net:add(nn.LogSoftMax())
